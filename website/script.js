682ed22d005b353941555c52a932f4ee6953923f
naziaali1525 <38943101+naziaali1525@users.noreply.github.com>
2021-03-23 23:02:28 +0500

    Add files via upload

---
+++ website/script.js
@@ -0,0 +1,9 @@
+function showIcon(){
+    var x = document.getElementById('show-list');
+    if (x.style.display === "none"){
+        x.style.display = "block";
+    }else{
+        x.style.display = "none";
+    }
+    
+}
